06e9c8e23741c4c7ca249ba17ab896de132a8d0d
daniyalmujtaba <daniyal.mujtaba@triplek.tech>
2021-09-13 14:27:14 +0500

    bye endpoint added

--- api/controllers/home.js
+++ api/controllers/home.js
@@ -3,3 +3,9 @@ exports.sayHello = (req,res,next) => {
     res.send('Hello from test code!')
     
 }
+
+
+exports.sayBye = (req,res,next) => {
+    res.send('bye from test code!')
+    
+}
--- api/routes/home.js
+++ api/routes/home.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const HomeController = require('../controllers/home');
 
 router.get("/hello" , HomeController.sayHello);
-    
-// router.post("/hello" , HomeController.getHello);
+
+router.get("/bye" , HomeController.sayBye);
 
 module.exports = router;
